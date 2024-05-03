fdab6110341b771d1ec80e5d5a73174efe0f5b47
Aliaksandr Stsiarzhanau <sterzhanoff@gmail.com>
2024-05-03 20:16:01 +0200

    BST: implement tree traversal methods (BFS and DFS variations)

--- src/data-structures/binary-search-tree/binarySearchTree.spec.ts
+++ src/data-structures/binary-search-tree/binarySearchTree.spec.ts
@@ -1,8 +1,38 @@
 import { it, expect, describe, beforeEach } from 'vitest';
 import { BinarySearchTree } from './binarySearchTree';
-// import { BinarySearchTreeNode } from './binarySearchTreeNode';
 
 describe('BST', () => {
+    let bstWithNodes: BinarySearchTree;
+
+    beforeEach(() => {
+        /**
+         *  Test BST stucture:
+         *                            ________________ 41 ________________
+         *                           |                                    |
+         *                 _________ 20 __________            __________  65 ___________
+         *                |                       |          |                          |
+         *          ____ 11 ____                  22         50                         91
+         *         |            |
+         *      __ 4 __      __ 13
+         *     |       |    |
+         *     2       6    12
+         */
+
+        bstWithNodes = new BinarySearchTree();
+        bstWithNodes.insert(41);
+        bstWithNodes.insert(20);
+        bstWithNodes.insert(65);
+        bstWithNodes.insert(11);
+        bstWithNodes.insert(22);
+        bstWithNodes.insert(50);
+        bstWithNodes.insert(91);
+        bstWithNodes.insert(4);
+        bstWithNodes.insert(13);
+        bstWithNodes.insert(2);
+        bstWithNodes.insert(6);
+        bstWithNodes.insert(12);
+    });
+
     const bst = new BinarySearchTree();
     expect(bst.root).toBeNull();
 
@@ -10,9 +40,11 @@ describe('BST', () => {
         it('should insert a new node with a specified value in a proper position in a tree', () => {
             /**
              *  Expected BST stucture:
-             *                   41
-             *          20                  65
-             *  11                      50          91
+             *                            ________________ 41 ________________
+             *                            |                                   |
+             *                 _________ 20                       __________  65 ___________
+             *                |                                   |                         |
+             *                11                                 50                         91
              */
 
             const bst = new BinarySearchTree();
@@ -78,27 +110,66 @@ describe('BST', () => {
         });
     });
 
-    // shoul behave like `Array.prototype.includes`
-    describe('find', () => {
-        let bst: BinarySearchTree;
-
-        beforeEach(() => {
-            bst = new BinarySearchTree();
-            bst.insert(41);
-            bst.insert(65);
-            bst.insert(20);
-            bst.insert(91);
-            bst.insert(50);
-            bst.insert(11);
-        });
-
+    describe('includes', () => {
         it('should return `true` if the node with a given value is present in the tree', () => {
-            expect(bst.find(41)).toBe(true);
-            expect(bst.find(20)).toBe(true);
+            expect(bstWithNodes.includes(41)).toBe(true);
+            expect(bstWithNodes.includes(20)).toBe(true);
         });
 
         it('should return `false` if the node with a given value is not present in the tree', () => {
-            expect(bst.find(100)).toBe(false);
+            expect(bstWithNodes.includes(100)).toBe(false);
+        });
+    });
+
+    describe('BFS', () => {
+        it("should return an empty array if there's no root node", () => {
+            const bst = new BinarySearchTree();
+            expect(bst.BFS()).toEqual([]);
+        });
+
+        it('should return an array containing all the tree nodes in correct order', () => {
+            expect(bstWithNodes.BFS()).toEqual([
+                41, 20, 65, 11, 22, 50, 91, 4, 13, 2, 6, 12,
+            ]);
+        });
+    });
+
+    describe('DFSPreOrder', () => {
+        it("should return an empty array if there's no root node", () => {
+            const bst = new BinarySearchTree();
+            expect(bst.DFSPreOrder()).toEqual([]);
+        });
+
+        it('should return an array containing all the tree nodes in correct order', () => {
+            expect(bstWithNodes.DFSPreOrder()).toEqual([
+                41, 20, 11, 4, 2, 6, 13, 12, 22, 65, 50, 91,
+            ]);
+        });
+    });
+
+    describe('DFSPostOrder', () => {
+        it("should return an empty array if there's no root node", () => {
+            const bst = new BinarySearchTree();
+            expect(bst.DFSPostOrder()).toEqual([]);
+        });
+
+        it('should return an array containing all the tree nodes in correct order', () => {
+            expect(bstWithNodes.DFSPostOrder()).toEqual([
+                2, 6, 4, 12, 13, 11, 22, 20, 50, 91, 65, 41,
+            ]);
+        });
+    });
+
+    describe('DFSInOrder', () => {
+        it("should return an empty array if there's no root node", () => {
+            const bst = new BinarySearchTree();
+            expect(bst.DFSInOrder()).toEqual([]);
+        });
+
+        it('should return an array containing all the tree nodes in correct order', () => {
+            expect(bstWithNodes.DFSInOrder()).toEqual([
+                2, 4, 6, 11, 12, 13, 20, 22, 41, 50, 65, 91,
+            ]);
         });
     });
 });
--- src/data-structures/binary-search-tree/binarySearchTree.ts
+++ src/data-structures/binary-search-tree/binarySearchTree.ts
@@ -43,8 +43,7 @@ export class BinarySearchTree {
         return this;
     }
 
-    // should behave like `Array.prototype.includes`
-    find(value: number) {
+    includes(value: number) {
         if (!this.root) {
             return false;
         }
@@ -62,5 +61,102 @@ export class BinarySearchTree {
         }
 
         return false;
+    }
+
+    BFS() {
+        if (!this.root) {
+            return [];
+        }
+
+        const queue: BinarySearchTreeNode[] = [this.root];
+        const data: number[] = [];
+        let node: BinarySearchTreeNode | null;
+
+        while (queue.length > 0) {
+            node = queue.shift()!;
+            data.push(node.value);
+
+            if (node.left) {
+                queue.push(node.left);
+            }
+
+            if (node.right) {
+                queue.push(node.right);
+            }
+        }
+
+        return data;
+    }
+
+    DFSPreOrder() {
+        if (!this.root) {
+            return [];
+        }
+
+        const data: number[] = [];
+
+        function traverse(node: BinarySearchTreeNode) {
+            data.push(node.value);
+
+            if (node.left) {
+                traverse(node.left);
+            }
+
+            if (node.right) {
+                traverse(node.right);
+            }
+        }
+
+        traverse(this.root);
+
+        return data;
+    }
+
+    DFSPostOrder() {
+        if (!this.root) {
+            return [];
+        }
+
+        const data: number[] = [];
+
+        function traverse(node: BinarySearchTreeNode) {
+            if (node.left) {
+                traverse(node.left);
+            }
+
+            if (node.right) {
+                traverse(node.right);
+            }
+
+            data.push(node.value);
+        }
+
+        traverse(this.root);
+
+        return data;
+    }
+
+    DFSInOrder() {
+        if (!this.root) {
+            return [];
+        }
+
+        const data: number[] = [];
+
+        function traverse(node: BinarySearchTreeNode) {
+            if (node.left) {
+                traverse(node.left);
+            }
+
+            data.push(node.value);
+
+            if (node.right) {
+                traverse(node.right);
+            }
+        }
+
+        traverse(this.root);
+
+        return data;
     }
 }
